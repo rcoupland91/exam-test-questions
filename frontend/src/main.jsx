35def33d0745d93b6958ee4b6131d076bb233070
Ryan Coupland <ryan@ryansmedia.xyz>
2026-03-18 08:26:01 +0000

    fix crypto random uuid issue on http

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,13 @@
+// crypto.randomUUID is only available in secure contexts (HTTPS/localhost).
+// This polyfill makes the PWA work over HTTP on local networks.
+if (typeof crypto !== 'undefined' && !crypto.randomUUID) {
+  crypto.randomUUID = () => {
+    return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, c =>
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+  };
+}
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
